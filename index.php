f4b39168481f7a054afac3a620893c1873de24b3
Frederic Bergeron <fbergeron@FBMacBookPro2017-2.local>
2020-03-27 13:38:59 -0400

    Connect database

--- index.php
+++ index.php
@@ -1,1 +1,33 @@
-Allo 
+<?php
+$servername = "127.0.0.1";
+$username = "root";
+$password = "newpassword";
+$db = "tourisme-express";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
+
+$sql = "SELECT id, prenom, nom FROM usager";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "<br>id: " . $row["id"]. " - Nom: " . $row["prenom"]. " " . $row["nom"]. "";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
+?>
+
+<?php
+   // $result = $mysqli->query("SELECT prenom FROM usager");
+	//	echo($result);
+?>
